624113e9a881dcca4a2311cbc8610cbf9795fbf3
Franks555 <63602861+Franks555@users.noreply.github.com>
2022-12-07 10:35:57 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,34 +1,12 @@
-// -------------- Declarations -------------- 
 
-let dificulty = 0;
-const arrayAnswers = [];
-const randomsWordsArray = [];
-let allAnswerCorrects = "True";
-let numberOfwords = 3;
-let index = 1;
-let time= 0;
-let rounds = 1;
+// Declarations
 
+let computerSelection = "";
+let playerSelection = "";
+let computerWinning = 0;
+let playerWinning = 0;
 
-
-const wordsArray = ["gap", "gold", "myth", "discourage", "merit", "recognize", "penalty", "cousin", "fare", "fair", "cater", "formulate", "skeleton", "laboratory", "castle", "gregarious", "sermon", "jury", "photograph",
-                "deny", "smell", "disposition", "notebook", "machinery", "spontaneous", "pay", "diplomatic", "turkey", "tumble", "make", "hope", "exposure", "thin", "agriculture", "basic", "reception", "disk",
-                "letter", "loop", "fantasy", "articulate", "mastermind", "mushroom", "morsel", "listen", "gravel", "health", "settle", "pudding", "dilemma", "resident", "license", "correspond", "attention", "suspicion",
-                "hunter", "mark", "drill", "seal", "spoil", "tire", "preparation", "spray", "usefull", "attitude", "reach", "wave", "sustain", "honor", "elapse", "wriggle", "contract", "live", "friend", "representative",
-                "turn", "multiply", "analyst", "dialogue", "laborer", "displace", "reader", "cafe", "absorption", "socialist", "priority", "harmful", "offense", "trial", "risk", "charter", "mayor", "appendix", "omission",
-                "supplementary", "dine", "peace"];
-         
-                
-// -------------- Functions -------------- 
-
-
-function createRandomArray(array1, array2){
-        while( array1.length ) {      
-                let = i = Math.floor( Math.random()*array1.length );
-                array2[array1.length]= array1.splice( i, 1 );
-        }
-}
-
+// Functions
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
@@ -36,91 +14,124 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
-
-function showWords(array, wordsNumber, time) {
-        let top = getRandomInt(0,1400);
-        let left = getRandomInt(0,1400); 
-        setTimeout(function() {
-            index++;   
-            const windowWord = window.open("", "",`top=${top},left=${left},width=200,height=100`);
-            windowWord.document.write(`<style>h1{color:red; text-align:center;}</style><h1>${array[index]}</h1>`);
-            setTimeout(function() {windowWord.close()}, time);        
-            if (index <= wordsNumber) {         
-                showWords(array, wordsNumber, time);            
-        }  
-      }, time)
+function computerPlay(){
+    let selection = getRandomInt(1,4);
+    switch (selection){
+        case 1 : computerSelection = "ROCK";
+                break;
+        case 2 : computerSelection = "PAPER";
+                break;
+        case 3 : computerSelection = "SCISSORS";
+                break;
+        default: 
+                console.log("machine didn't select a option");
+    }
 }
 
-
-function selectDificulty(){
-    while (dificulty != "Easy" || dificulty != "Normal" || dificulty != "Hard" ){
-        dificulty = prompt("Please, select level of dificulty of the MemoTest: \n \n 1. Easy \n \n 2. Normal \n \n 3. Hard \n \n");
-        if (dificulty == "Easy") {
-            time = 2000;
-            alert(" \n EASY dificulty selected. \n \n Are you ready??");
-            showWords(randomsWordsArray, numberOfwords, time);
+function userSelection(){
+    let inputSelection = "";
+    while (inputSelection != "ROCK" || inputSelection != "PAPER" || inputSelection != "SCISSORS" || inputSelection != "EXIT"){
+        inputSelection = prompt("Please, select a choise between Rock, Paper or Scissors:");
+        inputSelection = inputSelection.toUpperCase();
+        console.log(inputSelection);
+        if (inputSelection == "EXIT") {
+            playerSelection = "EXIT";
             break;
         }
-        if (dificulty == "Normal") {
-            time = 1400
-            alert(" \n NORMAL dificulty selected \n \n Are you ready??");
-            showWords(randomsWordsArray, numberOfwords, time);
+        if (inputSelection == "ROCK") {
+            playerSelection = "ROCK";
             break;
         }
-        if (dificulty == "Hard") {
-            time = 900;
-            alert(" \n HARD dificulty selected \n \n Are you ready??");
-            showWords(randomsWordsArray, numberOfwords, time);
+        if (inputSelection == "PAPER") {
+            playerSelection = "PAPER";
+            break;
+        }
+        if (inputSelection == "SCISSORS") {
+            playerSelection = "SCISSORS";
             break;
         }
         else{
-            alert("the option is invalid, the only values accepted are Easy, Normal and Hard. Please enter a the dificulty");
+            alert("the option is invalid, please enter the word Rock, Paper or Scissors");
+        }
+    }
+    
+}
+
+function playRound (userChoise, computerChoise){
+    if (userChoise == "EXIT"){
+        console.log("user left the game");
+    }
+    if (userChoise == "ROCK"){
+        switch (computerChoise){
+            case "ROCK":{
+                return "it's a tie!";
+            }
+            case "PAPER":{
+                computerWinning++;
+                return "You Lose! Paper beats Rock!";
+            }
+            case "SCISSORS":{
+                playerWinning++;
+                return "You Win! Rock beats Scissors!";
+            }
+        }
+    }   
+    if (userChoise == "PAPER"){
+            switch (computerChoise){
+                case "ROCK":{
+                    playerWinning++;
+                    return "You Win! Paper beats Rock";
+                }
+                case "PAPER":{
+                    return "it's a tie!";
+                }
+                case "SCISSORS":{
+                    computerWinning++;
+                    return "You Lose! Scissors beats Paper!";
+                }
+            }
+    }
+    if (userChoise == "SCISSORS"){
+        switch (computerChoise){
+            case "ROCK":{
+                computerWinning++;
+                return "You Lose! Rock beats Scissors";
+            }
+            case "PAPER":{
+                playerWinning++;
+                return "You Win! Scissors beats Paper";
+            }
+            case "SCISSORS":{
+                return "it's a tie!";
+            }
         }
     }
 }
 
-                             
-function compareArrays(array1, array2, length){
-    let i = 0;
-    let z = 2;
-    while (allAnswerCorrects && i < length){
-        if (array1[i] != array2[z]){
-            allAnswerCorrects = "False";
-        };
-        i++;
-        z++;
+function game(){
+
+    for (let i = 0; i < 5; i++){
+        computerPlay();
+        userSelection();
+        if (playerSelection == "EXIT"){
+            break;
+        }
+        else{ 
+            playRound(playerSelection, computerSelection);
+        }
+        
     }
-    console.log(allAnswerCorrects);
+    if (computerWinning > playerWinning){
+        console.log("Computer Wins!");
+    }
+    else if (computerWinning < playerWinning){
+        console.log("Player Wins!");
+    }
+    else {
+        console.log("it's a tie!");
+    }
 }
 
-function userAnswers(array){
-    const answers = [];
-    for (let i = 0; i < numberOfwords; i++){
-        answers [i] = prompt(`Enter the word N° ${i+1}`);
-    }
-    compareArrays(answers, array, numberOfwords);
-    if (allAnswerCorrects == "True"){
-        const result = window.open("", "","top=300,left=700,width=300,height=100");
-        result.document.write("<style>body{background-color:green;}h1{color:white; text-align:center;}</style><h1>You Win!</h1>");
-        console.log("entro por el if");
-    }
-    else{
-        const result2 = window.open("", "","top=300,left=700,width=300,height=100");
-        result2.document.write("<style>body{background-color:red;}h1{color:black; text-align:center;}</style><h1>Game Over..</h1>");
-    }
-} 
+// Excecutions
 
-function showUserPrompt(){
-    setTimeout(() => {
-        userAnswers(randomsWordsArray);
-    }, time*4.5);
-}
-
-
-// -------------- Execution -------------- 
-
-
-alert("˚̥̐  ο ° ˚̥̐  ο  °҉ ҈ ჻◦ᵒᵒ◦˚̥̐  ο ° ˚̥̐  °҉ ҈჻◦°҉ᵒᵒ◦˚̥̐  ο °\n◦˚̥̐   ₋ ₋ ₋ ₋ ₋ ₋ ₋ ₋ ₋ ₋ ₋ ₋ ₋ ₋ ₋ ₋ ₋ ₋ ₋ ₋  ҈჻◦\n◦°◦|              MEMO TEST               | ᵒᵒ\nο°|   →Trainning your Memory!← |◦˚̥̐҈ \n ᵒᵒ   ˉ ˉ ˉ ˉ ˉ ˉ ˉ ˉ ˉ ˉ ˉ ˉ ˉ ˉ ˉ ˉ ˉ ˉ ˉ ˉ ˉ  ᵒ◦˚̐  \n Welcome to MEMOTEST! \n In this game, your memory will be challenged. You need to remeber the words appairing on the screen and then writining! \n Good Luck!");
-createRandomArray(wordsArray, randomsWordsArray);
-selectDificulty(numberOfwords);
-showUserPrompt();
+game();
